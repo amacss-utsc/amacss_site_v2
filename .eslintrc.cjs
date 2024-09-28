e31390186ee8038f91bd87052c84eb9109535a16
Murphy Lee <murphylee2004@gmail.com>
2024-09-28 18:30:15 -0400

    feat: PR checks and pre-commit formatting & linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,30 @@
 module.exports = {
-  extends: 'next',
   root: true,
+  extends: [
+    'next/core-web-vitals',
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['unused-imports', '@typescript-eslint'],
+  rules: {
+    'unused-imports/no-unused-imports': 'warn',
+    'no-duplicate-case': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-func-assign': 'error',
+  },
+  globals: {
+    JSX: 'readonly',
   },
 }
