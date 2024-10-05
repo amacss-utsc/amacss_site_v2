8fa64502ece614f3e9b3d4a614b651ad5925ab0e
Murphy Lee <murphylee2004@gmail.com>
2024-10-05 16:23:13 -0400

    [TECH-10] Migrations for team structure

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241005_201938 from './20241005_201938'
+
+export const migrations = [
+  {
+    up: migration_20241005_201938.up,
+    down: migration_20241005_201938.down,
+    name: '20241005_201938',
+  },
+]
