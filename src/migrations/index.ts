482ab14864bafe38bcc694ef954f8d6e694943c3
Shreyas Rao <raoshreyas2004@gmail.com>
2024-10-09 19:30:13 -0400

    [TECH-16] small fixes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241005_201938 from './20241005_201938'
 import * as migration_20241006_010803 from './20241006_010803'
+import * as migration_20241009_232859 from './20241009_232859'
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20241006_010803.up,
     down: migration_20241006_010803.down,
     name: '20241006_010803',
+  },
+  {
+    up: migration_20241009_232859.up,
+    down: migration_20241009_232859.down,
+    name: '20241009_232859',
   },
 ]
