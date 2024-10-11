e1f21ea732492da9a8a3442364bd4ea914948ca6
m2zhang <marilyn2zhang@gmail.com>
2024-10-10 23:47:24 -0400

    Migrations
    Done by running pnpm payload migrate:create

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241011_034643 from './20241011_034643'
+
+export const migrations = [
+  {
+    up: migration_20241011_034643.up,
+    down: migration_20241011_034643.down,
+    name: '20241011_034643',
+  },
+]
